645f4442d08f3f7687a36b93329bdf5f64060f4f
Ian Norton <inorton@gmail.com>
2016-06-22 18:39:14 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
     version="001",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
-    author_email="ian.norton@thales-esecurity.com",
-    url="",
+    author_email="inorton@gmail.com",
+    url="https://github.com/inorton/junit2html",
     packages=["junit2htmlreport"],
     package_data={"junit2htmlreport": files},
     scripts=["junit2html"],
+    platforms=["any"],
+    license="License :: OSI Approved :: MIT License",
     long_description="Genearate a single file HTML report from a Junit XML file"
 )
