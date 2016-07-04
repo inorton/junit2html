2f97f6e822a02dc1bce52ff919d5a96eb17165a6
Ian Norton <inorton@gmail.com>
2016-07-04 07:20:41 +0100

    bump pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ files = ["*.css"]
 
 setup(
     name="junit2html",
-    version="002",
+    version="003",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
     author_email="inorton@gmail.com",
