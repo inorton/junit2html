1abcd20e35b8ffd7e779537893365cb763655a60
Ian Norton <inb@ncipher.com>
2016-11-23 11:49:10 +0000

    bump pypi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ files = ["*.css"]
 
 setup(
     name="junit2html",
-    version="004",
+    version="005",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
     author_email="inorton@gmail.com",
