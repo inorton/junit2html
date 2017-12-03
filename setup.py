20d2d7d1d1d61ce02fd5e5d7b196667d43563c03
inb <inorton@gmail.com>
2017-12-03 18:58:54 +0000

    bump to 011 to release python 3 fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ files = ["*.css"]
 
 setup(
     name="junit2html",
-    version="010",
+    version="011",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
     author_email="inorton@gmail.com",
