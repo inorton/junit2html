8e43a2a2011688980c405ea27432b3d8235faf4b
inb <inorton@gmail.com>
2017-12-14 09:19:48 +0000

    Allow running via `python -m junithtmlreport`

---
+++ junit2htmlreport/__main__.py
@@ -0,0 +1,2 @@
+import runner
+runner.start()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ files = ["*.css"]
 
 setup(
     name="junit2html",
-    version="011",
+    version="012",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
     author_email="inorton@gmail.com",
