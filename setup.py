166c7ecc7eb8d6b9872c0f6f6f37ff00158b41a4
Ian Norton <ian.norton@ncipher.com>
2020-08-17 12:39:17 +0100

    go back to old version scheme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ files = ["*.css"]
 
 setup(
     name="junit2html",
-    version="0.2.0",
+    version="25",
     description="Generate HTML reports from Junit results",
     author="Ian Norton",
     author_email="inorton@gmail.com",
