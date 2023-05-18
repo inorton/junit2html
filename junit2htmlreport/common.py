322679e471c3a004e41b8ae4b7a2a7f0296667a3
Aaron Holmes <aholmes@mednet.ucla.edu>
2023-05-18 14:45:27 -0700

    Add types to common.

--- junit2htmlreport/common.py
+++ junit2htmlreport/common.py
@@ -3,15 +3,21 @@ Common code between JUnit2HTML matrix and merge classes
 """
 from __future__ import print_function
 
+from typing import Any
+
+from junit2htmlreport.parser import Case, Junit
+
 
 class ReportContainer(object):
     """
     Hold one or more reports
     """
+    reports: dict[str, Junit]
+
     def __init__(self):
         self.reports = {}
 
-    def add_report(self, file):
+    def add_report(self, filename: str) -> Any:
         raise NotImplementedError()
 
     def failures(self):
@@ -19,7 +25,7 @@ class ReportContainer(object):
         Return all the failed test cases
         :return:
         """
-        found = []
+        found: list[Case] = []
         for report in self.reports:
             for suite in self.reports[report].suites:
                 found.extend(suite.failed())
@@ -31,7 +37,7 @@ class ReportContainer(object):
         Return all the skipped test cases
         :return:
         """
-        found = []
+        found: list[Case] = []
         for report in self.reports:
             for suite in self.reports[report].suites:
                 found.extend(suite.skipped())
